f818e78a2eef680d0794c0a764b9949f2cc36638
Brandi Lauria <n01057930@unf.edu>
2020-04-07 08:19:21 -0400

    Add project files.

---
+++ SC-Methods-IceCream1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SC_Methods_IceCream1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnComputePrice = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtScoops = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 20;
+            this.lstOutput.Location = new System.Drawing.Point(357, 17);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(218, 184);
+            this.lstOutput.TabIndex = 34;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(136, 219);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 28);
+            this.btnExit.TabIndex = 33;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(12, 219);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 28);
+            this.btnClear.TabIndex = 32;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnComputePrice
+            // 
+            this.btnComputePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComputePrice.Location = new System.Drawing.Point(156, 129);
+            this.btnComputePrice.Name = "btnComputePrice";
+            this.btnComputePrice.Size = new System.Drawing.Size(153, 28);
+            this.btnComputePrice.TabIndex = 31;
+            this.btnComputePrice.Text = "Compute Price";
+            this.btnComputePrice.UseVisualStyleBackColor = true;
+            this.btnComputePrice.Click += new System.EventHandler(this.btnComputePrice_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Unit Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Number of Scoops";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(171, 56);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(138, 26);
+            this.txtUnitPrice.TabIndex = 28;
+            // 
+            // txtScoops
+            // 
+            this.txtScoops.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScoops.Location = new System.Drawing.Point(171, 14);
+            this.txtScoops.Name = "txtScoops";
+            this.txtScoops.Size = new System.Drawing.Size(138, 26);
+            this.txtScoops.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(310, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Total Price";
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalPrice.Location = new System.Drawing.Point(410, 224);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(167, 26);
+            this.txtTotalPrice.TabIndex = 48;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 270);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnComputePrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.txtScoops);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnComputePrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.TextBox txtScoops;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+    }
+}
+
---
+++ SC-Methods-IceCream1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SC_Methods_IceCream1
+{
+    public partial class Form1 : Form
+    {
+
+        //Sample code with examples on using methods
+        //COP2010
+        //Doug Titus
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Methods are one of the main bulding bocks of programming
+        * Included here are some basic examples of how you could use a method
+        * 
+        * Using our IceCream sample code there is now a 
+        * simple method to calculate the order total
+        * 
+        * The method is called and code run by putting the 
+        * method name in the event handler
+        */
+
+        /* A method can be public or private 
+         * Private methods can only be accessed in the same class 
+         * Public methods can be accessed by other classes and forms 
+         */
+
+        int intScoopsQty = 0;
+        double dblUnitPrice = 0;
+        double dblSubtotal = 0;
+        double dblTotalPrice = 0;
+
+
+        private void btnComputePrice_Click(object sender, EventArgs e)
+        {
+            //input 
+            bool blnResultScoops = int.TryParse(txtScoops.Text, out intScoopsQty);
+            bool blnResultPrice = double.TryParse(txtUnitPrice.Text, out dblUnitPrice);
+
+            //calculate subtotal and display to listbox
+            dblSubtotal = intScoopsQty * dblUnitPrice;
+            lstOutput.Items.Add(dblSubtotal.ToString("c"));
+
+            //method to calculate total and display to textbox
+            CalculateTotal();
+
+        }//end method
+
+        //this method is void - that means it does not return a value
+        //it jbut just executes the code in the method and ends
+
+        //note: the variable type make a difference because of scope
+        //this only works because we are using field variables and not local
+        private void CalculateTotal()
+        {
+            //add subtotal and $1 fee to totalprice and output
+            dblTotalPrice = dblSubtotal + 1;
+            txtTotalPrice.Text = dblTotalPrice.ToString("c");
+        }//end method
+
+
+        //**************************************************************
+        
+        //calling a method from an event handler to clear text boxes and variables
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }//end method
+
+        //Below is a method that can be called from any event handler
+        //in the event handler you would add: clearAll();
+        private void ClearAll()
+        {
+            intScoopsQty = 0;
+            dblUnitPrice = 0;
+            dblTotalPrice = 0;
+            txtScoops.Clear();
+            txtUnitPrice.Clear();
+            lstOutput.Items.Clear();
+        }//end method
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }//end method
+    }//end class
+}//end namespace
